ee863979342b35ac324c14a2757d00d1608c60f9
Sofi <nsofiia01@gmail.com>
2022-12-04 14:51:42 -0600

    random initialization is moved outside of the loop

--- guessGame/Program.cs
+++ guessGame/Program.cs
@@ -9,13 +9,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void Main(string[] args)
         {
+            Random randomNumberGenerator = new Random();
             while (true)
             {
                 int wrongGuessCount = MAX_GUESSES;
-                Random randomNumberGenerator = new Random();
                 int randomNumber = randomNumberGenerator.Next(1, 101);
                 Console.WriteLine("Hello Traveler!");
-                //Console.WriteLine(randomNumber);
+                Console.WriteLine(randomNumber);
 
                 while (wrongGuessCount != 0)
                 {
